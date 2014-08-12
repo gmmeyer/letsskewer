9b3b42b1416e3bcb7f4fdb09e4255db45c4dbe4f
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-08-12 16:39:13 -0400

    fills out models and database entries

--- db/migrate/20140812195733_create_skewers.rb
+++ db/migrate/20140812195733_create_skewers.rb
@@ -1,8 +1,11 @@
 class CreateSkewers < ActiveRecord::Migration
   def change
     create_table :skewers do |t|
+      t.integer :user_id, null: false
+      t.integer :posts_count
 
       t.timestamps
     end
+    add_index :skewers, :user_id
   end
 end
--- db/migrate/20140812195738_create_posts.rb
+++ db/migrate/20140812195738_create_posts.rb
@@ -1,8 +1,14 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.text :body, null: false
+      t.integer :user_id
+      
+      t.integer, :skewer_id, null: false
 
       t.timestamps
     end
+    add_index :posts, :skewer_id
+    add_index :posts, :user_id
   end
 end
